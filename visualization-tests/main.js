1ae81a1ba805ac78aa6053a6d9ba758442d75d1f
Matias Castro <mjc7106@g.rit.edu>
2022-07-20 22:29:06 -0400

    mostly working version

--- visualization-tests/main.js
+++ visualization-tests/main.js
@@ -30,15 +30,6 @@ let raw_line_chart_data = [
 const MARKER_LEN = 10;
 raw_line_chart_data = u.mapRawData(raw_line_chart_data, 'gpm');
 
-function create_plateau(raw_data) {
-    var newData = [];
-    for (let i = 0; i < raw_data.length; i++) {
-        newData.push(raw_data[i]);
-        newData.push(raw_data[i]);
-        newData.push(raw_data[i]);
-    }
-    return newData;
-}
 
 // raw_line_chart_data = create_plateau(raw_line_chart_data);
 
@@ -53,13 +44,13 @@ let line_chart_data = Object.values(raw_line_chart_data.reduce(function(obj, val
 
 // console.log(Object.values(line_chart_data));
 
-
 line_chart(line_chart_data);
 
 
 //////////////////////////////////////////////////////////////////////////////
 
 function line_chart(data) {
+    let legendWidth = 60;
     var margin = { top: 100, right: 40, bottom: 40, left: 40 };
     var width = 800;
     var height = 600;
@@ -70,175 +61,331 @@ function line_chart(data) {
         .append("svg")
         .attr("width", width)
         .attr("height", height)
+        .attr("x", 0)
+        .attr("y", 0)
         .append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-
     let xMax = 100;
     let xMin = 2;
-    // var x = d3.scaleLinear()
-    //     .domain([0, xMax])
-    //     .range([0, chart_width]);
-    data = u.addXValues(data, xMax, data[0].length);
+
+    u.addXValues(data, xMax, data[0].length);
+    let colorMap = u.createColorMap(u.getUniqueTeamIds(data));
+    u.addColors(data, colorMap);
 
     var x = d3.scaleLinear()
         .domain([-xMin, xMax])
-        .range([0, chart_width]);
+        .range([legendWidth, chart_width]);
     
-    // Draws x axis on the bottom
-    // svg.append("g")
-    //     .attr("transform", `translate(0, ${chart_height})`)
-    //     .call(d3.axisBottom(x));
+    // Add Background Image
+    svg.append("bg")
+        .append("pattern")
+        .attr("id", "bg")
+        .attr('width', "100%")
+        .attr('height', "100%")
+        .attr('x', -xMin)
+        .attr('y', -10)
+        .append("image")
+        .attr('xlink:href', 'http://tali.gg-staging.s3-website.us-east-2.amazonaws.com/bg%202.png')
+        .attr('width', "100%")
+        .attr('height', "100%")
+        .attr('x', -xMin)
+        .attr('y', -2);
+    
+    // Draw Rect with BG Image
+    svg.append("rect")
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr("fill", "url(#bg)");
+    
+    
+    function drawOutlineOfEntireSVG(){
+        svg.append("rect")
+            .attr("width", "100%")
+            .attr("height", "100%")
+            .attr('fill', 'white')
+            .attr('opacity', '.7')
+            .attr('stroke', 'red');
+    }
+    // drawOutlineOfEntireSVG();
 
-    let flat = data.flat().map(e => e.y);
-    let min = Math.min(...flat);
-    let max = Math.max(...flat);
+    function drawHorizontalLine(yPoint){
+        svg.append("line")
+        .attr('class', 'horizontalLine')
+        .attr('stroke', 'white')
+        .attr('x1', x(xMin))
+        .attr('y1', y(yPoint))
+        .attr('x2', x(xMax))
+        .attr('y2', y(yPoint));
+    }
 
-    let trueMin = 400;
-    console.log(trueMin, max);
+    function writeHoriontalLineText(xPoint, yPoint){
+        // Each num is 5px wid
+        let sLen = String(yPoint).length; 
+        console.log(sLen);
+        if (sLen > 3){
+            xPoint = xPoint - 5/2;
+        }
+        svg.append('text')
+            .attr("x", xPoint)
+            .attr("y", y(yPoint) - 2)
+            .attr('fill', 'white')
+            .attr('font-family', 'Lexend Deca')
+            // .attr("text-anchor", "middle")
+            .attr("dominant-baseline", "central")
+            .text(yPoint);
+    }
+
+    let testMax = 2000;
+    let testMin = 0;
+
     var y = d3.scaleLinear()
         // Domain is for y is minY and maxY
-        .domain([0, max])
-        .range([chart_height, 0]);
+        .domain([testMin, testMax])
+        .range([chart_height, margin.top]);
 
-    // var y = d3.scaleLinear()
-    //     // Domain is for y is minY and maxY
-    //     .domain([3.5, 5.5])
-    //     .range([chart_height, 0]);
 
-    // Draws y axis on the left
-    // svg.append("g")
-    //     .call(d3.axisLeft(y));
+    function drawAxises(x, y){
+        svg.append("g")
+            .call(d3.axisLeft(y));
 
-    var color = d3.scaleOrdinal()
-        .domain([0, 1])
-        .range(d3.schemeSet2);
-
-    // color.domain(data.map(function (d) { return d.teamId; }));
-
-    // draws the lines
-    console.log(data);
-    var xValue = function(d){return d.x;}
-    var yValue = function(d){return d.y;}
-    
-    var lineFunction = d3.line()
-      .x(function(d) { return xValue(d); })
-      .y(function(d) { return yValue(d); });
-    //   .interpolate("linear");
-    
-    var line = d3.line()
-            .x(function(d){ return x(d.x)})
-            .y(function(d){return y(d.y)})
-
-    // var path = svg.append("path")
-    //           .attr({
-    //             "d":line(data),
-    //             "fill":"blue",
-    //             "stroke":"black"
-    //           })
-    
-    let yData = data.map(e => e.map(x => x.y));
-
-    let previousPoint = 0;
-    function test(point, index){
-        return point.x;
+        svg.append("g")
+            .attr("transform", `translate(0, ${chart_height})`)
+            .call(d3.axisBottom(x));
     }
 
-    function testy(point){
-        return point.y;
+    let padding = (legendWidth/2)/2
+
+    // Draw YAxis Legend Rect
+    svg.append("rect")
+        .attr("width", legendWidth)
+        .attr("height", chart_height - (margin.bottom + margin.top))
+        .attr("x", padding)
+        .attr("y", margin.top + padding)
+        .attr('fill', 'rgba(54, 49, 60, 0.8)');
+    
+    let horizontalLines = [0, 400, 800, 1200, 1600, 2000];
+    for(let i = 0; i < horizontalLines.length; i++){
+        drawHorizontalLine(horizontalLines[i]);
     }
 
-    function drawRect(point){
-        if (point.type == "mid"){
-            return point.x;
-        }
+    for(let i = 1; i < horizontalLines.length - 1; i++){
+        writeHoriontalLineText(legendWidth*.5, horizontalLines[i]);
     }
+
+    // drawAxises(x, y);
+
 
     let pData = u.createPlateau(data, xMin);
     console.log('pData', pData);
+
+    // Create Line Graph
     svg.selectAll(".line")
         .data(pData)
         .join("path")
-        .attr("fill", "none")
-        .attr("stroke", color)
+        .attr("fill", 'none')
+        // .attr("background", (d) => u.getAreaFill(d))
+        .attr("stroke", (d) => u.getLineColor(d))
         .attr("stroke-width", 5)
-        .attr("d", d => d3.line()
-            // This is an interpolater and fills in the points between the points
-            // we passed to the graph
-            // with out it the line is jagged, with it the line is curved
-            // .curve(d3.curveLinear)
-            .x((point, index) => x(test(point, index)))
-            .y((point) => y(testy(point)))
-            (d));
+        .attr("d", (d) => d3.line()
+            .x((point, index) => x(u.xPoint(point)))
+            .y((point) => y(u.yPoint(point)))
+            (d))
+        .attr('class', (d) => "line" + d[0].teamId);
 
-    let ttDiv = d3.select("#line_chart").append("div")
+
+    // Defs tells D3 this is not an element
+    var defs = svg.append('defs');
+
+    // Create necessary linear gradients
+    function createLinearGradients(){
+        defs.append("linearGradient")
+            .attr("id", "orange-area-gradient")
+            .attr("x1", '0%')
+            .attr("y1", '0%')
+            .attr("x2", '0%')
+            .attr("y2", '100%')
+            .selectAll("stop")
+            .data([
+                {offset: "19.15%", color: "rgba(112, 106, 97, 1)"},
+                {offset: "67.35%", color: "rgba(25, 28, 49, 0)"}
+            ])
+            .enter().append("stop")
+            .attr("offset", function(d) { return d.offset; })
+            .attr("stop-color", function(d) { return d.color; });
+
+    defs.append("linearGradient")
+            .attr("id", "purple-area-gradient")
+            .attr("gradientUnits", "userSpaceOnUse")
+            .attr("x1", '0%')
+            .attr("y1", '0%')
+            .attr("x2", '0%')
+            .attr("y2", '100%')
+            .selectAll("stop")
+            .data([
+                {offset: "19.33%", color: "rgba(145, 137, 156, 1)"},
+                {offset: "86%", color: "rgba(25, 28, 49, 0)"}
+            ])
+            .enter().append("stop")
+            .attr("offset", function(d) { return d.offset; })
+            .attr("stop-color", function(d) { return d.color; });
+
+    defs.append("linearGradient")
+            .attr("id", "hover-gradient")
+            .attr("gradientUnits", "userSpaceOnUse")
+            .attr("x1", '0%')
+            .attr("y1", '0%')
+            .attr("x2", '0%')
+            .attr("y2", '100%')
+            .selectAll("stop")
+            .data([
+                {offset: ".33%", color: "#FCA311"},
+                {offset: "54.54%", color: "rgba(255, 255, 255, 0)"}
+            ])
+            .enter().append("stop")
+            .attr("offset", function(d) { return d.offset; })
+            .attr("stop-color", function(d) { return d.color; });
+    }
+
+    createLinearGradients();
+
+
+    // Create Area Graph
+    svg.selectAll(".area")
+        .data(pData)
+        .join("path")
+        .attr("fill", (d) => `url(#${u.getAreaColorId(d)})`) //;'url(#area-gradient)')
+        // .attr("background", (d) => u.getAreaFill(d))
+        .attr("d", (d) => d3.area()
+            .x((point) => x(u.xPoint(point)))
+            .y1((point) => y(u.yPoint(point)))
+            .y0(y(0))
+            (d))
+        .attr('class', (d) => "area" + d[0].teamId);
+
+
+    // Create Different Colored Tooltips
+    let orangeToolTip = d3.select("#line_chart").append("div")
         .attr("class", "toolTip")
-        .style("opacity", 10);
+        .style("background", "linear-gradient(89.99deg, rgba(254, 179, 56, 0.25) 0.01%, rgba(255, 255, 255, 0) 100%)")
+        .style("border", "0.5px solid #FDE4BA")
+        .style("opacity", 100);
 
+    let purpleToolTip = d3.select("#line_chart").append("div")
+        .attr("class", "toolTip")
+        .style("background", "linear-gradient(89.99deg, rgba(220, 204, 239, 0.25) 0.01%, rgba(255, 255, 255, 0) 100%)")
+        .style("border", "0.5px solid rgba(226, 201, 255, 1)")
+        .style("opacity", 100);
+    
+    // Creating marker hover rects
+    let orangeHoverRect = d3.select('#line_chart').append('div')
+        .attr("class", "hover_rect")
+        .style("opacity", 100)
+        .style("background", "linear-gradient(179.96deg, #FCA311 0.03%, rgba(255, 255, 255, 0) 54.54%)");
+
+    let purpleHoverRect = d3.select('#line_chart').append('div')
+        .attr("class", "hover_rect")
+        .style("opacity", 100)
+        .style("background", "linear-gradient(179.96deg, #BC96E6 0.03%, rgba(255, 255, 255, 0) 54.54%)");
+
+    u.addToolTips(pData, orangeToolTip, purpleToolTip );
+    
+
+    var backgroundElement = document.getElementById('bg');
+    var bgPos = backgroundElement.getBoundingClientRect();
+
+    const markerWidth = 20;
+    const markerHeight = 12;
+
+    const markerWidthOnHover = 24;
+    const markerHeightOnHover = 14;
     let markers = pData.map(x => x.filter(y => {return y.type == "mid"}));
-    console.log(markers);
+
+    // Draw markers
     svg.selectAll(".rects")
         .data(markers)
         .join("g")
-        .attr("fill", color)
+        .attr("fill", (d) => u.getMarkerColor(d))
         .selectAll(".rect_points")
         .data(set => set)
         .join("svg:rect")
         .attr("rx", (d, i) => 3)
         .attr("ry", (d, i) => 1)
-        .attr("x", (d, i) => x(test(d) -1.3))
-        .attr("y", (d) => y(testy(d) + 20))
-        .attr("width", (x) => 20)
-        .attr("height", (x) => 10)
+        .attr("x", (d, i) => u.markerXPosition(d, markerWidth, x))
+        .attr("y", (d) => u.markerYPosition(d, markerHeight, y))
+        .attr("width", (d) => markerWidth)
+        .attr("height", (d) => markerHeight)
+        .attr("class", (d) => "marker" + d.color.colorName)
+        .attr("id", (d) => u.makeMarkerId(d))//"marker" + d.color.colorName + "y" + d.y + "x" + d.x)
         .on("mouseover", function(e, d) {
-            ttDiv.transition()
+            let hoverRect;
+            if (d.color.colorName === u.ORANGE_NAME){
+                hoverRect = orangeHoverRect;
+            } else {
+                hoverRect = purpleHoverRect;
+            }
+
+            hoverRect
+                .style("left", x(d.x) + margin.left  + "px")
+                .style("top", (bgPos.top) + u.markerYPosition(d, markerHeight, y) + (markerWidth/2) - 2 + "px")
+                .transition()
                 .duration(200)
-                .style("opacity", .9);
-            ttDiv.html("GPM: " + d.y)
-                // .style("left", (d.x) + "px")
-                .style("left", (e.x) + "px")
-                // .style("top", (d.y - 28) + "px");
-                .style("top", (200) + "px");
+                // .style("top", y(d.y) + margin.top + margin.bottom + "px")
+                .style("opacity", 90);
+
+            let marker = svg.select('#' + u.makeMarkerId(d));
+            marker 
+                .attr('fill', u.getMarkerHoverColor(d))
+                .attr("x", u.markerXPosition(d, markerWidthOnHover, x))
+                .attr("y", u.markerYPosition(d, markerHeightOnHover, y))
+                .attr('width', markerWidthOnHover)
+                .attr('height', markerHeightOnHover);
+            
+
+            let toolTip = d.toolTip;
+            toolTip.transition()
+                .duration(200)
+                .style("opacity", 100)
+            toolTip.html(u.createToolTipText(d))
+                .style("left", bgPos.left + margin.left  + "px")
+                .style("top", bgPos.top *1.1  + "px");
+            
         })
-        .on("mouseout", function(d) {
-            ttDiv.transition()
-                .duration(500)
+        .on("mouseout", function(e, d) {
+            let hoverRect;
+            if (d.color.colorName === u.ORANGE_NAME){
+                hoverRect = orangeHoverRect;
+            } else {
+                hoverRect = purpleHoverRect;
+            }
+
+            hoverRect
+                .transition()
+                .duration(0)
                 .style("opacity", 0);
+
+            let marker = svg.select('#' + u.makeMarkerId(d));
+            let toolTip = d.toolTip;
+            toolTip.transition()
+                .duration(50)
+                .style("opacity", 0)
+
+            marker
+                .attr('fill', u.getMarkerColor(d))
+                .attr("x", u.markerXPosition(d, markerWidth, x))
+                .attr("y", u.markerYPosition(d, markerHeight, y))
+                .attr('width', markerWidth)
+                .attr('height', markerHeight);
+            
+
         });;
 
-    // .attr("transform", function(d, i) { return "scale(" + (1 - d / 25) * 20 + ")"; });
-    // .style("fill", d3.scale.category20c());
-
-
-    // .attr("transform", function(d, i) { return "scale(" + (1 - d / 25) * 20 + ")"; });
-    //     // .attr("r", 5)
-    //     .attr("stroke", "white");
-
-    // svg.selectAll(".dots")
-    //     .data(data)
-    //     .join("g")
-    //     .attr("fill", (d, i) => color(i))
-    //     .selectAll(".points")
-    //     .data(set => set)
-    // .join("svg:rect")
-    // .attr("rx", (d, i) => x(i))
-    // .attr("ry", (d) => y(d))
-    // .attr("x", (d, i) => x(i))
-    // .attr("y", (d, i) => y(d))
-    // .attr("width", 1)
-    // .attr("height", 1)
-    // .attr("transform", function(d, i) { return "scale(" + (1 - d / 25) * 20 + ")"; })
-    // .attr("stroke", "white");
-
-    // .style("fill", d3.scale.category20c());
-
-    // svg.append("svg:rect")
-    //     .attr("rx", 6)
-    //     .attr("ry", 6)
-    //     .attr("x", -12.5)
-    //     .attr("y", -12.5)
-    //     .attr("width", 25)
-    //     .attr("height", 25)
-    //     // .attr("transform", function(d, i) { return "scale(" + (1 - d / 25) * 20 + ")"; })
-    //     .style("fill", d3.scale.category20c());
+        // Get text size
+        // var fontSize = 12;
+        // var test = document.getElementById("Test");
+        // test.style.fontSize = fontSize;
+        // var height = (test.clientHeight + 1) + "px";
+        // var width = (test.clientWidth + 1) + "px"
+        
+        // console.log(height, width);
 }
--- visualization-tests/utils.js
+++ visualization-tests/utils.js
@@ -1,4 +1,17 @@
 
+export const PURPLE_NAME = "purple";
+export const ORANGE_NAME = "orange";
+const PURPLE = 'rgba(145,137,156,100)';
+const ORANGE = 'rgba(153,145,133,100)';
+const M_PURPLE = 'rgba(234,197,251,.5)';
+const M_ORANGE = 'rgba(255,241,202,.8)';
+const T_PURPLE = 'rgba(241, 228, 255, 1)';
+const T_ORANGE = '#FDE4BA';
+const AREA_GRADIENT_ID_PURPLE = 'purple-area-gradient';
+const AREA_GRADIENT_ID_ORANGE = 'orange-area-gradient';
+const H_ORANGE = 'rgba(252, 163, 17, 1)';
+const H_PURPLE = 'rgba(188, 150, 230, 1)';
+
 
 export function mapRawData(data, yKey){
     let newData = [];
@@ -43,14 +56,12 @@ export function createPlateau(data, pointPadding){
 }
 
 export function addXValues(data, xMax, intervals){
-    let newData = [];
     let interval = xMax/intervals;
-    let xValues = [];
+    let xValues = [interval/2];
 
-    for (let i = 0; i < intervals; i++){
-        xValues.push(i*interval);
+    for (let i = 2; i < intervals + 1; i++){
+        xValues.push((i*interval) - interval/2);
     }
-    console.log('xValues', xValues);
 
     for (let i = 0; i < data.length; i++){
         let rows = data[i].sort((x1, x2) => x1 - x2);
@@ -60,7 +71,111 @@ export function addXValues(data, xMax, intervals){
         for (let j = 0; j < rows.length; j++){
             rows[j].x = xValues[j];
         }
-        newData.push(rows)
     }
-    return newData; 
+}
+
+export function addColors(data, colorMap){
+    for (let i = 0; i < data.length; i++){
+        let rows = data[i];
+        let color = colorMap[rows[0].teamId];
+        for (let j = 0; j < rows.length; j++){
+            rows[j].color = color;
+        }
+    }
+}
+
+export function createColorMap(teamIds){
+    let colorMap = {};
+    for (let i = 0; i < teamIds.length; i++){
+        if (i === 0){
+            colorMap[teamIds[i]] = {
+                lineColor: PURPLE,
+                markerColor: M_PURPLE,
+                colorName: PURPLE_NAME,
+                textColor: T_PURPLE,
+                areaColor: AREA_GRADIENT_ID_PURPLE,
+                hoverColor: H_PURPLE,
+            }
+        } else {
+            colorMap[teamIds[i]] = {
+                lineColor: ORANGE,
+                markerColor: M_ORANGE,
+                colorName: ORANGE_NAME,
+                textColor: T_ORANGE,
+                areaColor: AREA_GRADIENT_ID_ORANGE,
+                hoverColor: H_ORANGE,
+            }
+        }
+    }
+    return colorMap;
+}
+
+export function getUniqueTeamIds(data){
+    let flat = Array.prototype.concat.apply([], data);
+    return [...new Set(flat.map(d => d.teamId))];
+}
+
+export function addToolTips(data, orangeToolTip, purpleToolTip){
+    for (let i = 0; i < data.length; i++){
+        let rows = data[i];
+        for (let j = 0; j < rows.length; j++){
+            let row = rows[j];
+            if (row.color.colorName == PURPLE_NAME){
+                row.toolTip = purpleToolTip;
+            } else {
+                row.toolTip = orangeToolTip;
+            }
+        }
+    }
+}
+
+export function createToolTipText(data){
+    return `<span class="toolTip" style="color:${data.color.textColor}">
+    GPM: ${data.y} </br>
+    Opponent: ${data.teamId}
+    </span> 
+    `
+    
+}
+
+export function makeMarkerId(d){
+    return "marker" + d.color.colorName + "y" + d.y + "x" + d.x;
+}
+
+export function getLineColor(d){
+    return d[0].color.lineColor;
+}
+
+export function getMarkerColor(d){
+    if (d.constructor === Array){
+        return d[0].color.markerColor;
+    } else {
+        return d.color.markerColor;
+    }
+}
+
+export function getMarkerHoverColor(d){
+    return d.color.hoverColor;
+}
+
+export function getAreaColorId(d){
+    let x =  d[0].color.areaColor;
+    console.log(x);
+    return x;
+}
+
+export function xPoint(point){
+    return point.x;
+}
+
+export function yPoint(point){
+    return point.y;
+}
+
+export function markerXPosition(point, markerWidth, xScale){
+    return xScale(xPoint(point)) - markerWidth/2;
+}
+
+export function markerYPosition(point, markerHeight, yScale){
+    return yScale(yPoint(point)) - markerHeight/2;
 }
